0988fa56b7966ecf6d01b419a58cd8b57fb985f2
Francisco Brito <franciscojavierbrito16@gmail.com>
2023-11-25 16:42:36 -0300

    final code for pass the test for the token model

--- spec/controllers/users/login_spec.rb
+++ spec/controllers/users/login_spec.rb
@@ -16,9 +16,13 @@ RSpec.describe V1::UsersController, type: :controller do
                         subject { response }
                         it { is_expected.to have_http_status(:ok) }
                   end
-                  context 'Respuesta de login correcta' do
+                  context 'Estructura de respuesta login correcta' do
                         subject { playload_test }
-                        it { is_expected.to include(:id, :email, :age, :store) }
+                        it { is_expected.to include(:id, :email, :age, :store, :token) }
+                  end
+                  context 'Estructura de respuesta de token correcta' do
+                        subject { playload_test[:token] }
+                        it { is_expected.to include(:id, :token, :expires_at) }
                   end
             end
             context 'Inicio de sesión fallido' do
