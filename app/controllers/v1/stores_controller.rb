4d5670026120896e108a09e3743d398da3bf6611
Francisco Brito <franciscojavierbrito16@gmail.com>
2023-11-25 18:39:06 -0300

    adding code for pass tests show controller in store model

--- app/controllers/v1/stores_controller.rb
+++ app/controllers/v1/stores_controller.rb
@@ -1,6 +1,17 @@
 module V1 
       class StoresController < ApplicationController
-            def show
+            before_action :authenticate_user!
+            before_action :set_store 
+
+            def show; end
+
+            private
+            def set_store
+                  if @current_user.store_id.to_s == params[:id]
+                        @store = @current_user.store
+                  else
+                        head :unauthorized
+                  end
             end
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[create] do
         post 'login', on: :collection
       end
+      resources :stores
     end
   end
 end
