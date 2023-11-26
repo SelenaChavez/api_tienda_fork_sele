f3acbc1ca6a8f5ce210253a87d5b3031f5ba15bf
Francisco Brito <franciscojavierbrito16@gmail.com>
2023-11-25 21:56:49 -0300

    adding tests for the products controller

---
+++ spec/controllers/products/create_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe V1::ProductsController, type: :controller do
+      describe "Registro de productos" do
+            let(:user) { create(:owner) } 
+            let(:bearer) { create(:token, user: user) }
+            let(:headers) { { Authorization: "Bearer #{bearer.token}" } }
+            let(:product) {
+                  {
+                        store_id: user.store_id,
+                        name: Faker::Book.title,
+                        description: Faker::Lorem.sentence(word_count: 50),
+                        price: rand(1..100)
+                  }
+            }
+            context "Registro del producto correctamente" do
+                  before do
+                        request.headers.merge! headers
+                        post(:create, format: :json, params: { product: product })
+                  end
+                  context "Respuesta con status de created" do
+                        subject { response }
+                        it { is_expected.to have_http_status(:created) } 
+                  end
+                  context "Estructura correcta del producto" do
+                        subject { playload_test }
+                        it { is_expected.to include(:id, :name, :description, :price, :created_at, :updated_at, :store_id) }
+                  end 
+            end
+            context "Registro del producto fallido" do
+                  before do
+                        product[:name] = ''
+                        request.headers.merge! headers
+                        post(:create, format: :json, params: { product: product })
+                  end
+                  context "Respuesta con status de bad request" do
+                        subject { response } 
+                        it { is_expected.to have_http_status(:bad_request) }
+                  end
+                  context "Estructura correcta del listado de errores" do
+                        subject { playload_test } 
+                        it { is_expected.to include(:errors) }
+                  end
+            end
+            context "Registro de producto sin token" do
+                  before do
+                        post(:create, format: :json, params: { product: product })
+                  end 
+                  context "Respuesta con status (unauthorized)" do
+                        subject { response } 
+                        it { is_expected.to have_http_status(:unauthorized) }
+                  end
+                  
+            end
+            
+      end 
+end
